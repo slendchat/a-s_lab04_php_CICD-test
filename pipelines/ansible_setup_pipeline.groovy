7f3f35e4642098c3bedf1eb1b404dd5754e7b59e
Artur Voitkovskii <54188589+slendchat@users.noreply.github.com>
2025-11-17 19:33:46 +0200

    Update ansible_setup_pipeline.groovy

--- pipelines/ansible_setup_pipeline.groovy
+++ pipelines/ansible_setup_pipeline.groovy
@@ -6,14 +6,13 @@ pipeline {
             steps {
                 withCredentials([
                     sshUserPrivateKey(
-                        credentialsId: 'ssh-key-agent',
-                        keyFileVariable: 'SSH_KEY',
-                        usernameVariable: 'SSH_USER'
+                        credentialsId: 'ansible-private-key',
+                        keyFileVariable: 'SSH_KEY'
                     )
                 ]) {
                     sh """
                     docker exec ansible-agent bash -c '
-                        export ANSIBLE_PRIVATE_KEY_FILE=$SSH_KEY
+                        export ANSIBLE_PRIVATE_KEY_FILE=/home/ansible/.ssh/id_ansible
                         ansible-playbook \
                             -i /home/ansible/work/hosts.ini \
                             /home/ansible/work/setup_test_server.yml
