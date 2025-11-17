a53cfa7a1efff1186056e8ee18df0f7aef12f6d9
Artur Voitkovskii <54188589+slendchat@users.noreply.github.com>
2025-11-17 20:41:40 +0200

    Update ansible_setup_pipeline.groovy

--- pipelines/ansible_setup_pipeline.groovy
+++ pipelines/ansible_setup_pipeline.groovy
@@ -15,7 +15,7 @@ pipeline {
             steps {
                 sh """
                     export PATH="/opt/venv/bin:\$PATH"
-
+                    export ANSIBLE_HOST_KEY_CHECKING=False
                     /opt/venv/bin/ansible-playbook \\
                         -i /home/ansible/ansible/hosts.ini \\
                         /home/ansible/ansible/setup_test_server.yml
