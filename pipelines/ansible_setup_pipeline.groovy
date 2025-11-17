9011dfa076f975351a0c09b27cc03e1c90e07e84
Artur Voitkovskii <54188589+slendchat@users.noreply.github.com>
2025-11-17 19:35:05 +0200

    Refactor Ansible setup pipeline stages

--- pipelines/ansible_setup_pipeline.groovy
+++ pipelines/ansible_setup_pipeline.groovy
@@ -2,23 +2,25 @@ pipeline {
     agent { label 'ssh-agent' }
 
     stages {
-        stage('Run Ansible Setup') {
+
+        stage('Clone Ansible Repository') {
             steps {
-                withCredentials([
-                    sshUserPrivateKey(
-                        credentialsId: 'ansible-private-key',
-                        keyFileVariable: 'SSH_KEY'
-                    )
-                ]) {
-                    sh """
+                sh '''
+                    rm -rf ansible_repo
+                    git clone https://github.com/slendchat/a-s_lab04_php_CICD-test.git ansible_repo
+                '''
+            }
+        }
+
+        stage('Run Ansible Playbook') {
+            steps {
+                sh """
                     docker exec ansible-agent bash -c '
-                        export ANSIBLE_PRIVATE_KEY_FILE=/home/ansible/.ssh/id_ansible
                         ansible-playbook \
                             -i /home/ansible/work/hosts.ini \
                             /home/ansible/work/setup_test_server.yml
                     '
-                    """
-                }
+                """
             }
         }
     }
